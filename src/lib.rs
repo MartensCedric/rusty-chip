604c01459f813d6c939a0b4bbdfd9c72e218ac83
Cedric Martens <cedricmartens98@gmail.com>
2020-11-08 23:05:00 -0500

    Add configuration

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,25 @@
-pub mod rusty_chip {
+use std::env;
+use std::error::Error;
+use std::fs;
 
-    use std::env;
-    use std::error::Error;
-    use std::fs;
+pub struct Config {
+    pub rom_filename: String,
+}
 
-    pub struct Config {
-        pub rom_filename: String,
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 2 {
+            return Err("not enough arguments");
+        }
+
+        let rom_filename = args[1].clone();
+
+        Ok(Config { rom_filename })
     }
+}
 
-    pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-        let contents = fs::read_to_string(config.rom_filename)?;
-        println!("Started rusty_chip!");
-        Ok(())
-    }
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.rom_filename)?;
+    println!("Started rusty_chip!");
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+use rusty_chip::Config;
 use std::env;
 use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem with arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Hello, world!");
 }
