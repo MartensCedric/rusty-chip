8e999a2c12e6e47fe9343c078e3568e6d1d84785
Cedric Martens <cedricmartens98@gmail.com>
2020-11-10 22:48:18 -0500

    Add fundamental chip8 properties

--- src/chip8.rs
+++ src/chip8.rs
@@ -1,4 +1,12 @@
-pub struct chip8 {}
+pub struct chip8 {
+    memory: [u8;4096],
+    cpu_registers: [u8;16],
+    index_register: u16,
+    program_counter: u16,
+    gfx: [u8; 64 * 32],
+    delay_timer: u8,
+    sound_timer: u8,
+}
 
 impl chip8 {
     pub fn execute_instruction(opcode: u8, arg1: u8, arg2: u8, arg3: u8) {}
